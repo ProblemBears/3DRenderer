4298cf531165cfb5e5ac9e2e51680433591c37d9
Jonathan <jonathan6md123@yahoo.com>
2025-03-09 19:44:48 -0400

    3.a - Creating an SDL Window

--- 3DRenderer/Main.c
+++ 3DRenderer/Main.c
@@ -1,8 +1,44 @@
 #include <stdio.h>
+#include <stdbool.h>
 #include <SDL3/SDL.h>
 
+bool is_running = false;
+SDL_Window* window = NULL;
+SDL_Renderer* renderer = NULL;
+
+bool initialize_window(void) {
+	if (SDL_Init(SDL_INIT_VIDEO) != 0) {
+		fprintf(stderr, "Error initializing SDL .\n");
+		return false;
+	}
+
+	// Create a SDL Window
+	window = SDL_CreateWindow(
+		NULL,
+		SDL_WINDOWPOS_CENTERED,
+		SDL_WINDOWPOS_CENTERED,
+		800,
+		600,
+		SDL_WINDOW_BORDERLESS
+	);
+	if (!window) {
+		fprintf(stderr, "Error creating SDL window. \n");
+		return false;
+	}
+
+	// Create a SDL Renderer
+	renderer = SDL_CreateRenderer(window, -1, 0);
+	if (!renderer) {
+		fprintf(stderr, "Error creating SDL renderer. \n");
+		return false;
+	}
+
+	return true;
+}
+
 int main(int argc, char* args[]) {
-	SDL_Init(SDL_INIT_AUDIO | SDL_INIT_VIDEO | SDL_INIT_JOYSTICK | SDL_INIT_HAPTIC | SDL_INIT_GAMEPAD | SDL_INIT_EVENTS | SDL_INIT_SENSOR | SDL_INIT_CAMERA);
-	printf("Hello, world!");
+
+	is_running = initialize_window();
+
 	return 0;
 }
