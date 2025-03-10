c493e9cbeff8ec8ee0791eadf133238471a28bd4
Jonathan <jonathan6md123@yahoo.com>
2025-03-09 20:24:06 -0400

    3.b - Rendering a SDL Window

--- 3DRenderer/Main.c
+++ 3DRenderer/Main.c
@@ -7,38 +7,66 @@ SDL_Window* window = NULL;
 SDL_Renderer* renderer = NULL;
 
 bool initialize_window(void) {
-	if (SDL_Init(SDL_INIT_VIDEO) != 0) {
+	if (SDL_Init(SDL_INIT_VIDEO) == 0) {
 		fprintf(stderr, "Error initializing SDL .\n");
 		return false;
 	}
 
-	// Create a SDL Window
-	window = SDL_CreateWindow(
-		NULL,
-		SDL_WINDOWPOS_CENTERED,
-		SDL_WINDOWPOS_CENTERED,
-		800,
-		600,
-		SDL_WINDOW_BORDERLESS
-	);
-	if (!window) {
-		fprintf(stderr, "Error creating SDL window. \n");
-		return false;
-	}
-
-	// Create a SDL Renderer
-	renderer = SDL_CreateRenderer(window, -1, 0);
-	if (!renderer) {
-		fprintf(stderr, "Error creating SDL renderer. \n");
+	// Create the window and renderer using SDL_CreateWindowAndRenderer
+	if (SDL_CreateWindowAndRenderer(NULL, 800, 600, SDL_WINDOW_BORDERLESS, &window, &renderer) == 0) {
+		fprintf(stderr, "Error creating SDL window and renderer: %s\n", SDL_GetError());
 		return false;
 	}
 
 	return true;
 }
 
+void setup(void)
+{
+
+}
+
+void process_input(void)
+{
+	SDL_Event event;
+	SDL_PollEvent(&event);
+
+	switch (event.type) {
+	case SDL_EVENT_QUIT:
+		is_running = false;
+		break;
+	case SDL_EVENT_KEY_DOWN:
+		if (event.key.key == SDLK_ESCAPE)
+			is_running = false;
+		break;
+	}
+}
+
+void update(void)
+{
+
+}
+
+void render(void)
+{
+	SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+	SDL_RenderClear(renderer);
+
+	SDL_RenderPresent(renderer);
+}
+
 int main(int argc, char* args[]) {
 
 	is_running = initialize_window();
+
+	setup();
+
+	while (is_running)
+	{
+		process_input();
+		update();
+		render();
+	}
 
 	return 0;
 }
